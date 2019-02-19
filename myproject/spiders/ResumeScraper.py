9bc19d7d80decc00660d8d21bc3abf64e57b1ac3
kaftand <kaftand@gmail.com>
2019-02-19 12:40:11 -0600

    initial commit

---
+++ myproject/spiders/ResumeScraper.py
@@ -0,0 +1,27 @@
+import scrapy
+from .. import items
+
+class ResumeScraper(scrapy.Spider):
+    name = "myResumeScraper"
+
+    def start_requests(self):
+        yield scrapy.Request(url = "http://localhost/resumeLinks.html", callback=self.parseResumeLists)
+
+    def parseResumeLists(self, response):
+        links = response.css("ul#resumeStuff a::attr(href)").extract()
+        for link in links:
+            yield response.follow(url = link, callback = self.parse)
+
+    def parse(self, response):
+        if "head" in response.url.lower():
+            tag = "head"
+        else:
+            tag = "body"
+        thisItem = items.MyprojectItem()
+        thisItem["text"] = response.text
+        thisItem["tag"] = tag
+        yield thisItem
+
+
+
+
